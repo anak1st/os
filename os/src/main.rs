4ac781f3ad5394172e30b5bc87e513c8c5e6ea03
Anak1st <87300894+anak1st@users.noreply.github.com>
2024-01-11 21:09:16 +0800

    finish ch1 basic os

--- os/src/main.rs
+++ os/src/main.rs
@@ -1,9 +1,36 @@
 #![no_std]
 #![no_main]
+#![feature(panic_info_message)]
+
+
 use core::arch::global_asm;
 
 
+#[macro_use]
+mod console;
 mod lang_items;
+mod sbi;
 
 
 global_asm!(include_str!("entry.asm"));
+
+
+/// clear BSS segment
+fn clear_bss() {
+    extern "C" {
+        fn sbss();
+        fn ebss();
+    }
+    (sbss as usize..ebss as usize).for_each(|a| {
+        unsafe { (a as *mut u8).write_volatile(0) }
+    });
+}
+
+
+/// the rust entry-point of os
+#[no_mangle]
+pub fn rust_main() -> ! {
+    clear_bss();
+    println!("Hello, world!");
+    panic!("Shutdown machine!");
+}
